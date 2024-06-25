f80e2466a540de1d5da1970189ac142d3fa90ff3
krisce <kcamachoe559@ulacit.ed.cr>
2024-06-25 16:05:55 -0600

    f conf

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -1,10 +1,11 @@
 import { useState } from "react";
 import appFirebase from "../credenciales";
-import { getAuth,onAuthStateChanged } from "firebase/auth";
+import { getAuth,onAuthStateChanged } from "firebase/auth"
+import { getFirestore, doc, getDoc } from "firebase/firestore";
 
-import login from "../src/componenents/Login"
-
-import Home from "../src/componenents/Home"
+// componentes 
+import login from "../components/Login";
+import Home from "../src/components/Home"
 
 import './App.css'
 const auth = getAuth(appFirebase )
@@ -13,10 +14,15 @@ function App(){
 
     const [usuario, setUsuario] = useState(null)
 
-    onAuthStateChanged(auth,(usuarioFirebase)=>)
-    if(usuarioFirebase){
+    onAuthStateChanged(auth,(usuarioFirebase)=>{
+        if(usuarioFirebase){
         setUsuario(usuarioFirebase)
-    }
+        }
+        else
+        {
+            setUsuario(null)
+        }
+    })
 return(
 <div>
     {usuario ? <Home correusuario = {usuario.email} /> : <login/>}
