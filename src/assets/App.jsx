f771c1849a3c0fbee25a240f7fbe9954b2bc726a
krisce <kcamachoe559@ulacit.ed.cr>
2024-06-25 15:49:06 -0600

    conf fire base

--- src/assets/App.jsx
+++ src/assets/App.jsx
@@ -1,17 +1,27 @@
 import { useState } from "react";
 import appFirebase from "../credenciales";
 import { getAuth,onAuthStateChanged } from "firebase/auth";
-const auth = getAuth(appFirebase )
 
+import login from "../src/componenents/Login"
+
+import Home from "../src/componenents/Home"
 
 import './App.css'
+const auth = getAuth(appFirebase )
 
 function App(){
 
+    const [usuario, setUsuario] = useState(null)
+
+    onAuthStateChanged(auth,(usuarioFirebase)=>)
+    if(usuarioFirebase){
+        setUsuario(usuarioFirebase)
+    }
 return(
 <div>
+    {usuario ? <Home correusuario = {usuario.email} /> : <login/>}
 
-    
+
 </div>
 
 
@@ -20,3 +30,6 @@ return(
 
 
 }
+
+export default App
+
