2cf77fa06b2b7eed837fdbed6561f458f2af9b54
krisce <kcamachoe559@ulacit.ed.cr>
2024-06-26 15:36:42 -0600

    fix

---
+++ src/utils/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Bienvenido a mi aplicación React</h1>
+      </header>
+      <main>
+        <p>Esta es una aplicación React básica.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
